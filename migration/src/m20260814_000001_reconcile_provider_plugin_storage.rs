970d538e429e7949525f3bbd8f04197d92d5879c
anuragRusty <anuragsingh2002dec@gmail.com>
2026-08-14 14:15:50 +0530

    fix: discard legacy provider plugin data

--- migration/src/m20260814_000001_reconcile_provider_plugin_storage.rs
+++ migration/src/m20260814_000001_reconcile_provider_plugin_storage.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Perter Technology Solutions Private Limited
 // SPDX-License-Identifier: Apache-2.0
 
-use sea_orm_migration::{prelude::*, sea_orm::ConnectionTrait};
+use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -9,14 +9,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        for table in ["provider_credentials", "provider_plugins"] {
-            if table_has_rows(manager, table).await? {
-                return Err(DbErr::Custom(format!(
-                    "legacy table '{table}' contains data; migrate it to ai_engines before removing the table"
-                )));
-            }
-        }
-
         manager
             .drop_table(
                 Table::drop()
@@ -42,22 +34,4 @@ impl MigrationTrait for Migration {
             .up(manager)
             .await
     }
-}
-
-async fn table_has_rows(manager: &SchemaManager<'_>, table: &str) -> Result<bool, DbErr> {
-    if !manager.has_table(table).await? {
-        return Ok(false);
-    }
-
-    let query = Query::select()
-        .expr(Expr::value(1))
-        .from(Alias::new(table))
-        .limit(1)
-        .to_owned();
-    let db = manager.get_connection();
-
-    Ok(db
-        .query_one(db.get_database_backend().build(&query))
-        .await?
-        .is_some())
 }
